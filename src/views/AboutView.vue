6f03c67517c1922d1a8f7e249f3b8f78c14aa791
wangxin0 <wangin0@egova.com.cn>
2024-05-10 23:27:56 +0800

    feat:auto-import 配置

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>系统文件221213目录</h1>
+    <ul>
+      <li v-for="(file, index) in files" :key="index">{{ file }}</li>
+      <el-button type="primary" @click="getSystemFiles">获取系统文件</el-button>
+    </ul>
+    <el-card style="max-width: 480px">
+      <template #header>
+        <div class="card-header">
+          <span>Card name</span>
+        </div>
+      </template>
+      <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+      <template #footer>Footer content</template>
+    </el-card>
+    <el-divider></el-divider>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script>
+export default {
+  data() {
+    return {
+      files: []
+    }
+  },
+  mounted() {
+    // 调用Node.js中的方法来获取系统文件目录
+    this.getSystemFiles()
+    ElMessage.error('This is an error message')
+  },
+  methods: {
+    getSystemFiles() {
+      const fs = window.require('fs')
+      const directoryPath = '/' // 这里可以更改为你想要获取的目录路径
+
+      // 使用fs模块读取目录内容
+      fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+          console.error('Error reading directory:', err)
+        } else {
+          this.files = files
+        }
+      })
+    }
   }
 }
-</style>
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,27 @@ import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import { ElectronDevPlugin } from './plugins/vite.electron.dev'
 import { ElectronBuildPlugin } from './plugins/vite.electron.build'
-// https://vitejs.dev/config/
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [vue(), VueDevTools(), ElectronDevPlugin(), ElectronBuildPlugin()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+    ElectronDevPlugin(),
+    ElectronBuildPlugin(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      resolvers: [ElementPlusResolver()],
+      dts: './auto-imports.d.ts',
+      eslintrc: { enabled: false }
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+      dts: './components.d.ts'
+    })
+  ],
   base: './', //默认绝对路径,改成相对路径,不然会白屏
   resolve: {
     alias: {
